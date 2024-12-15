7bf3f99043c5df5f97bc7c1aedd8edc438472d80
SeungYongChoi <ppyooy336@naver.com>
2024-12-15 16:02:48 +0900

    feat: LetterResponse에 fontType letterPapaerType 추가

--- src/main/java/com/toy/namoneo/letter/controller/dto/response/LetterResponse.java
+++ src/main/java/com/toy/namoneo/letter/controller/dto/response/LetterResponse.java
@@ -1,6 +1,8 @@
 package com.toy.namoneo.letter.controller.dto.response;
 
 import com.toy.namoneo.letter.domain.Letter;
+import com.toy.namoneo.letter.domain.enums.FontType;
+import com.toy.namoneo.letter.domain.enums.LetterPaperType;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,15 +12,17 @@ public class LetterResponse {
     private String id;
     private String letterReceiver;
     private String letterSender;
-    private String text;
+    private String message;
     private String imageUrl;
+    private FontType fontType;
+    private LetterPaperType letterPaperType;
 
     public static LetterResponse from(Letter entity, String fullPathUrl) {
         return LetterResponse.builder()
                 .id(entity.getId())
                 .letterReceiver(entity.getLetterReceiver())
                 .letterSender(entity.getLetterSender())
-                .text(entity.getMessage())
+                .message(entity.getMessage())
                 .imageUrl(fullPathUrl)
                 .build();
     }

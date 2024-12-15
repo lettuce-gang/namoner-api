851a1c6f5289d516536a24c002d6a9d5781581c6
SeungYongChoi <ppyooy336@naver.com>
2024-12-15 16:47:23 +0900

    feat: CORS 설정 (테스트 서버 배포임으로 모든 요청 허용)

--- src/main/java/com/toy/namoneo/common/web/WebConfig.java
+++ src/main/java/com/toy/namoneo/common/web/WebConfig.java
@@ -2,6 +2,7 @@ package com.toy.namoneo.common.web;
 
 import com.toy.namoneo.common.logs.LoggingInterceptor;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -13,5 +14,17 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoggingInterceptor())
                 .order(1)
                 .addPathPatterns("/**");
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        //todo 정식 배포 시 수정 필요
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Authorization", "Content-Type")
+                .exposedHeaders("Custom-Header")
+                .allowCredentials(true)
+                .maxAge(3600);
     }
 }

08ffc6c1c7b1230188f3a5dd0e6eb4d10f401072
won983212 <won983212@naver.com>
2025-02-19 23:13:59 +0900

    feat: [#75] 전화번호 암호화 처리

--- src/main/java/com/toy/namoner/domain/auth/service/AuthService.java
+++ src/main/java/com/toy/namoner/domain/auth/service/AuthService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import com.toy.namoner.domain.auth.controller.dto.response.LoginResponse;
 import com.toy.namoner.domain.auth.controller.dto.response.NMNToken;
 import com.toy.namoner.common.jwt.JwtService;
+import com.toy.namoner.domain.user.model.UserDetail;
 import com.toy.namoner.infra.service.dto.OAuthUserInfo;
 import com.toy.namoner.domain.user.model.User;
 import com.toy.namoner.domain.user.service.UserService;
@@ -19,7 +20,7 @@ public class AuthService {
 	private final JwtService jwtService;
 
 	public LoginResponse loginByOAuthInfo(OAuthUserInfo info) {
-		User user = userService.findOrCreateByOAuth(info);
+		User user = userService.findOrCreateByUserJoin(UserDetail.fromOAuth(info));
 
 		NMNToken nmnToken = jwtService.generateToken(user);
 
